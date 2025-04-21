67e2fce8d0e510ae7076018bcce60ca39304402e
EfrnMC <emartinezc26@miumg.edu.gt>
2025-04-21 17:09:11 -0600

    EJERCICIO4.cpp

--- Lab3_Arreglos_Efren_Martínez/EJERCICIO4.cpp
+++ Lab3_Arreglos_Efren_Martínez/EJERCICIO4.cpp
@@ -1,79 +1,51 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-const int TAM = 15;
+int main() {
+    int numeros[15], mayores[15];
+    int suma = 0, j = 0;
 
-// Funcion para ingresar numeros
-void ingresarNumeros(int arr[]) {
-    cout << "Ingrese " << TAM << " numeros enteros:\n";
-    for (int i = 0; i < TAM; i++) {
-        cin >> arr[i];
+    // Ingreso de los números
+    cout << "Ingrese 15 numeros:\n";
+    for (int i = 0; i < 15; i++) {
+        cout << "Numero " << i + 1 << ": ";
+        cin >> numeros[i];
+        suma += numeros[i];
     }
-}
 
-// Funcion para calcular el promedio
-double calcularPromedio(int arr[]) {
-    int suma = 0;
-    for (int i = 0; i < TAM; i++) {
-        suma += arr[i];
-    }
-    return static_cast<double>(suma) / TAM;
-}
+    float promedio = suma / 15.0;
 
-// Funcion para crear un nuevo arreglo con valores mayores al promedio
-vector<int> mayoresAlPromedio(int arr[], double promedio) {
-    vector<int> mayores;
-    for (int i = 0; i < TAM; i++) {
-        if (arr[i] > promedio) {
-            mayores.push_back(arr[i]);
+    // Filtrar mayores al promedio
+    for (int i = 0; i < 15; i++) {
+        if (numeros[i] > promedio) {
+            mayores[j++] = numeros[i];
         }
     }
-    return mayores;
-}
 
-// Funcion para ordenar el arreglo de mayor a menor
-void ordenarDescendente(vector<int>& arr) {
-    sort(arr.begin(), arr.end(), greater<int>());
-}
-
-// Funcion para imprimir un arreglo
-void imprimirArreglo(const int arr[], int tam) {
-    cout << "Arreglo original:\n";
-    for (int i = 0; i < tam; i++) {
-        cout << arr[i] << " ";
+    // Ordenar los mayores al promedio (de mayor a menor)
+    for (int i = 0; i < j - 1; i++) {
+        for (int k = i + 1; k < j; k++) {
+            if (mayores[i] < mayores[k]) {
+                int temp = mayores[i];
+                mayores[i] = mayores[k];
+                mayores[k] = temp;
+            }
+        }
     }
+
+    // Mostrar resultados
+    cout << "\nPromedio: " << promedio << endl;
+
+    cout << "Arreglo original: ";
+    for (int i = 0; i < 15; i++) {
+        cout << numeros[i] << " ";
+    }
+
+    cout << "\nMayores al promedio (ordenados): ";
+    for (int i = 0; i < j; i++) {
+        cout << mayores[i] << " ";
+    }
+
     cout << endl;
-}
-
-// Funcion para imprimir un vector
-void imprimirVector(const vector<int>& arr) {
-    cout << "Arreglo de numeros mayores al promedio:\n";
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-    int numeros[TAM];
-
-    ingresarNumeros(numeros);
-
-    double promedio = calcularPromedio(numeros);
-    vector<int> mayores = mayoresAlPromedio(numeros, promedio);
-
-    imprimirArreglo(numeros, TAM);
-    imprimirVector(mayores);
-
-    // Extra: Ordenar el nuevo arreglo si hay elementos en el
-    if (!mayores.empty()) {
-        ordenarDescendente(mayores);
-        cout << "\nArreglo ordenado de mayor a menor:\n";
-        imprimirVector(mayores);
-    }
-
     return 0;
 }
